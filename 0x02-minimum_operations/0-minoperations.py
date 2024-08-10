623905b329a10f2a16500ded70175d34d8832ed8
vick-ram <vickramodero6@gmail.com>
2024-08-09 22:57:15 -0700

    minoperation ile

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters.
+    Args:
+        n: The desired number of H characters.
+    Returns:
+        An integer representing the minimum number of operations,
+        or 0 if n is impossible to achieve.
+    """
+
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    
+    return operations
